cc4a934dbc418940817640a8c0326066ee2b2b82
martin-mcinerney <martin_mcinerney@live.ie>
2022-11-18 20:51:50 +0000

    Adds some sprite file paths

--- game.js
+++ game.js
@@ -7,3 +7,14 @@ kaboom({
     debug: true,
     clearColor: [0, 0, 1, 1],
 })
+
+// Our sprites (the artwork that makes up the building blocks of the game)
+
+// sets the location of sprite files
+loadRoot('./')
+// regular box item
+loadSprite('music-note', 'music-note.png')
+// enemy sprite
+loadSprite('beer', 'beer.png')
+// special box item
+loadSprite('guitar', 'guitar.png')
