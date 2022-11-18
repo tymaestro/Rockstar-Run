6309dff1544e462252f94ec1e790c6255aa40976
Martin McInerney <98256205+martin-mcinerney@users.noreply.github.com>
2022-11-18 21:54:52 +0000

    Basic game settings and map rendered (#23)
    * Martin initial commit
    
    * Adds kaboom settings to game.js
    
    * Adds some sprite file paths
    
    * Basic map and game general config done
    
    Co-authored-by: martin-mcinerney <“martin_mcinerney@live.ie”>

--- game.js
+++ game.js
@@ -7,3 +7,51 @@ kaboom({
     debug: true,
     clearColor: [0, 0, 1, 1],
 })
+
+// Our sprites (the artwork that makes up the building blocks of the game)
+
+// sets the location of sprite files
+loadRoot('./')
+// regular box item
+loadSprite('music-note', 'music-note.png')
+// enemy sprite
+loadSprite('beer', 'beer.png')
+// special box item
+loadSprite('guitar', 'guitar.png')
+// floor block')
+loadSprite('block', 'block.png')
+
+// Game render settings
+scene("game", () => {
+    layers(['bg', 'obj', 'ui'], 'obj')
+
+    // These characters represent the sprites that can be found in legend
+    const map = [
+        '                                      ',
+        '                                      ',
+        '                                      ',
+        '                                      ',
+        '                                      ',
+        '                                      ',
+        '                                      ',
+        '                                      ',
+        '                         ^  ^         ',
+        '======================================',
+    ]
+
+
+    const levelCfg = {
+        width: 20,
+        height: 20,
+
+        // this is the legend for the sprites and defines the characteristics 
+        '=': [sprite('block', solid())],
+        '^': [sprite('beer'), solid(), 'dangerous'],
+
+    }
+    // defines the map/s that will be rendered for the level
+    const gameLevel = addLevel(map, levelCfg)
+})
+
+// guess what this does?
+start('game')
