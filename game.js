59fae70278aa4621346ac1765238180819058e3b
martin-mcinerney <martin_mcinerney@live.ie>
2022-11-19 10:45:27 +0000

    Score working merge fixed

--- game.js
+++ game.js
@@ -9,10 +9,6 @@ kaboom({
 })
 
 const MOVE_SPEED = 140
-const CURRENT_JUMP_FORCE = 700
-const BIG_JUMP_FORCE = 700
-
-const MOVE_SPEED = 140
 const JUMP_FORCE = 360
 let CURRENT_JUMP_FORCE = 700
 const BIG_JUMP_FORCE = 700
@@ -35,8 +31,6 @@ loadSprite('surprise-box', 'surprise-box.png')
 // rock
 // loadSprite('rock', 'rock.png')
 loadSprite('rockstar-girl', 'rockstar-girl.png')
-// loadSprite('rock', 'rock.png')
-loadSprite('rockstar-girl', 'rockstar-girl.png')
 
 // Game render settings
 scene("game", () => {
@@ -54,14 +48,11 @@ scene("game", () => {
         '                                      ',
         '                                      ',
         '                                      ',
-        '                                     ',
         '                             @        ',
         '                                      ',
         '            !                         ',
-        '            !                         ',
         '                   z                  ',
         '                                      ',
-        '       @                              ',
         '       @                              ',
         '                                      ',
         '                         ^  ^         ',
@@ -70,110 +61,22 @@ scene("game", () => {
 
 
     const levelCfg = {
-            width: 20,
-            height: 20,
+        width: 20,
+        height: 20,
 
-            // this is the legend for the sprites and defines the characteristics 
-            '=': [sprite('block'), solid()],
-            '=': [sprite('block'), solid()],
-            '^': [sprite('beer'), solid(), 'dangerous'],
-            '@': [sprite('surprise-box')),
-            solid(),
-            'guitar-surprise'],
-        '!': [sprite('surprise-box'), solid(, 'guitar-surprise'],
-            '!': [sprite('surprise-box'), solid(), 'note-surprise', 'note-surprise'],
-            'x': [sprite('guitar', solid()), 'guitar', body(), 'guitar', body()],
-            'z': [sprite('music-note'), solid(), 'note', ],
-            // // 'y': [sprite('rock', solid())],
+        // this is the legend for the sprites and defines the characteristics 
+        '=': [sprite('block'), solid()],
+        '^': [sprite('beer'), solid(), 'dangerous'],
+        '@': [sprite('surprise-box'), solid(), 'guitar-surprise'],
+        '!': [sprite('surprise-box'), solid(), 'note-surprise'],
+        'x': [sprite('guitar', solid()), 'guitar', body()],
+        'z': [sprite('music-note'), solid(), 'note'],
+        // 'y': [sprite('rock', solid())],
 
-        }
+
+    }
     // defines the map/s that will be rendered for the level
     const gameLevel = addLevel(map, levelCfg)
-
-    // these are the player settings. we need three of these functions for the three players in an if statement 
-    const player = add([
-        sprite('rockstar-girl'), solid(),
-        pos(30, 0),
-        body(),
-        big(),
-        origin('bot')
-    ])
-
-    // the logic that makes things jump out of boxes
-    player.on("headbump", (obj) => {
-        if (obj.is('note-surprise')) {
-            gameLevel.spawn('z', obj.gridPos.sub(0, 1))
-            destroy(obj)
-            gameLevel.spawn('}', obj.gridPos.sub(0, 0))
-        }
-        if (obj.is('guitar-surprise')) {
-            gameLevel.spawn('x', obj.gridPos.sub(0, 1))
-            destroy(obj)
-            gameLevel.spawn('}', obj.gridPos.sub(0, 0))
-        }
-    })
-    // the displayed score in game 
-    const scoreText = add([
-        text('I will be the score'),
-        pos(30, 6),
-        layer('ui'),
-        {
-            value: 'test',
-        }
-    ])
-
-    add([text('level' + 'test', pos(4, 6))])
-
-    keyDown('left', () => {
-        player.move(-MOVE_SPEED, 0)
-    })
-
-    keyDown('right', () => {
-        player.move(MOVE_SPEED, 0)
-    })
-
-    keyPress('space', () => {
-        if (player.grounded()) {
-            isJumping = true
-            player.jump(CURRENT_JUMP_FORCE)
-        }
-    })
-
-    // the logic that will allow us to get bigger when we touch a guitar
-    function big() {
-        let timer = 0
-        let isBig = false
-        return {
-            update() {
-                if (isBig) {
-                    CURRENT_JUMP_FORCE = BIG_JUMP_FORCE
-                    timer -= dt()
-                    if (timer <= 0) {
-                        this.smallify()
-                    }
-                }
-            },
-            isBig() {
-                return isBig
-            },
-            smallify() {
-                this.scale = vec2(1)
-                CURRENT_JUMP_FORCE = JUMP_FORCE
-                timer = 0
-                isBig = false
-            },
-            biggify(time) {
-                this.scale = vec2(2)
-                timer = time
-                isBig = true
-            }
-        }
-    }
-    // makes guitars move when they come out of a box
-    action('guitar', (m) => {
-        m.move(20, 0)
-    })
-
 
     // these are the player settings. we need three of these functions for the three players in an if statement 
     const player = add([
